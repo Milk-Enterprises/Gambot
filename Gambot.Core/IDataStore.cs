73dd5411a1acd97acbe8ad9f23036abd5ecb19e7
Mohammed Hossain <mohammedh123@gmail.com>
2014-09-24 20:10:17 -0400

    Moves IDataStore to Gambot.Core.

---
+++ Gambot.Core/IDataStore.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Gambot.Core
+{
+    public interface IDataStore
+    {
+        void Put(string key, string val);
+
+        void RemoveAllValues(string key);
+
+        void RemoveValue(string key, string val);
+
+        IEnumerable<string> GetAllValues(string key);
+
+        string GetRandomValue(string key);
+    }
+}
