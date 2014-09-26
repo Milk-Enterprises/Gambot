86c6ae146fe4d904c0a3bff429415b9cde2c98e3
Mohammed Hossain <mhossain@itbit.com>
2014-09-26 17:25:56 -0400

    Adds the IDataStoreManager interface.

---
+++ Gambot.Core/IDataStoreManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gambot.Core
+{
+    public interface IDataStoreManager
+    {
+        IDataStore Get(string name);
+    }
+}
