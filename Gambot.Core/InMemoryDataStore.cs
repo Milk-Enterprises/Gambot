558ca5436c43b492744833a7d16829a411cd010d
Mohammed Hossain <mohammedh123@gmail.com>
2014-09-26 20:31:07 -0400

    Adds InMemoryDataStore and InMemoryDataStoreManager.

---
+++ Gambot.Core/InMemoryDataStore.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gambot.Core
+{
+    internal class InMemoryDataStore : IDataStore
+    {
+        public bool Put(string key, string val)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int RemoveAllValues(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveValue(string key, string val)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetAllValues(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetRandomValue(string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Gambot.Core/InMemoryDataStoreManager.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+
+namespace Gambot.Core
+{
+    // todo: move this to the appropriate project/namespace
+    internal class InMemoryDataStoreManager : IDataStoreManager
+    {
+        private readonly IDictionary<string, IDataStore> dataStores;
+
+        public InMemoryDataStoreManager()
+        {
+            dataStores = new Dictionary<string, IDataStore>(); // todo: maybe use a ConcurrentDictionary but whos gives a shit really
+        }
+ 
+        public IDataStore Get(string name)
+        {
+            IDataStore dataStore;
+            var worked = dataStores.TryGetValue(name, out dataStore);
+            if (worked) return dataStore;
+
+            var newDataStore = new InMemoryDataStore();
+            dataStores[name] = newDataStore;
+
+            return newDataStore;
+        }
+    }
+}
