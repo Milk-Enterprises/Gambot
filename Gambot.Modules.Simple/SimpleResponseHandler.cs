e3011ead238e8a64c7a5f05e0f9ed61307b44500
Mohammed Hossain <mhossain@itbit.com>
2014-10-01 17:49:35 -0400

    Adds a new project/module for simple response handlers.

--- Gambot.Modules.Simple/SimpleResponseHandler.cs
+++ Gambot.Modules.Simple/SimpleResponseHandler.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 using Gambot.Core;
 
-namespace Gambot.SimpleResponses
+namespace Gambot.Modules.Simple
 {
     public class SimpleResponseHandler : IMessageHandler
     {
