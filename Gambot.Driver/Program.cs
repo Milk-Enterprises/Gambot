110d9be2f9245b9cc21cb407fe3bd4baf45e4ecf
Mohammed Hossain <mohammedh123@gmail.com>
2014-10-09 00:44:40 -0400

    Adds a QuotesModule to the project.

--- Gambot.Driver/Program.cs
+++ Gambot.Driver/Program.cs
@@ -8,6 +8,7 @@ using Gambot.Core;
 using Gambot.Data;
 using Gambot.Data.InMemory;
 using Gambot.IO.Console;
+using Gambot.Modules.Quotes;
 using Gambot.Modules.People;
 using Gambot.Modules.Reply;
 using Gambot.Modules.Variables;
---
+++ Gambot.Modules.Quotes/QuotesModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Gambot.Core;
+
+namespace Gambot.Modules.Quotes
+{
+    public class QuotesModule : AbstractModule
+    {
+        public QuotesModule()
+        {
+            var recentMessageStore = new RecentMessageStore(Int32.Parse(Config.Get("MaxMessagesRememberedPerUser"))); // todo: use tryparse + default value + logging
+
+            MessageHandlers.Add(new QuoteCommandHandler());
+            MessageHandlers.Add(new RememberCommandHandler(recentMessageStore));
+        }
+    }
+}
