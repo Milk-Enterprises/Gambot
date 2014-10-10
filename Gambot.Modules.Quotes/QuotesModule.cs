487344996de55eb96d8ae9ed3d0c5765296b954b
Mohammed Hossain <mohammedh123@gmail.com>
2014-10-10 00:28:14 -0400

    Updates the QuotesModule ctor.

--- Gambot.Modules.Quotes/QuotesModule.cs
+++ Gambot.Modules.Quotes/QuotesModule.cs
@@ -5,11 +5,13 @@ namespace Gambot.Modules.Quotes
 {
     public class QuotesModule : AbstractModule
     {
-        public QuotesModule()
+        public QuotesModule(IVariableHandler variableHandler)
         {
+            // todo: maybe move out to ioc, incase other shit need want this thing do?
             var recentMessageStore = new RecentMessageStore(Int32.Parse(Config.Get("MaxMessagesRememberedPerUser"))); // todo: use tryparse + default value + logging
 
-            MessageHandlers.Add(new QuoteCommandHandler());
+            MessageHandlers.Add(new RecentMessageListener(recentMessageStore));
+            MessageHandlers.Add(new QuoteCommandHandler(variableHandler));
             MessageHandlers.Add(new RememberCommandHandler(recentMessageStore));
         }
     }
