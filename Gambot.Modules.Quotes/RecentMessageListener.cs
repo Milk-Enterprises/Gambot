4cd71abad322123eb89131ae28bcef9b40a9b694
Mohammed Hossain <mohammedh123@gmail.com>
2014-10-10 00:27:52 -0400

    Adds a new message handler that listens to incoming messages and stores it in a recent message store.

---
+++ Gambot.Modules.Quotes/RecentMessageListener.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gambot.Core;
+using Gambot.Data;
+
+namespace Gambot.Modules.Quotes
+{
+    internal class RecentMessageListener : IMessageHandler
+    {
+        private readonly IRecentMessageStore recentMessageStore;
+
+        public RecentMessageListener(IRecentMessageStore recentMessageStore)
+        {
+            this.recentMessageStore = recentMessageStore;
+        }
+
+        public void Initialize(IDataStoreManager dataStoreManager)
+        {}
+
+        public bool Digest(IMessenger messenger, IMessage message, bool addressed)
+        {
+            recentMessageStore.AddMessageFromUser(message.Who, message);
+
+            return true;
+        }
+    }
+}
