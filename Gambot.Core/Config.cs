79c2bc1916c086733a20fd8d084d1ca481fbd9e2
Mohammed Hossain <mohammedh123@gmail.com>
2014-10-12 18:58:03 -0400

    Adds some logging to the Config class.

--- Gambot.Core/Config.cs
+++ Gambot.Core/Config.cs
@@ -3,20 +3,40 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Text;
+using NLog;
 
 namespace Gambot.Core
 {
     public static class Config
     {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
         public static string Get(string key, string defaultValue = null)
         {
-            return ConfigurationManager.AppSettings[key] ?? defaultValue;
+            logger.Info("Getting config value for \"{0}\"...", key);
+            var val = ConfigurationManager.AppSettings[key];
+            if (val == null) {
+                logger.Info("No value found for \"{0}\", defaulting value to \"{1}.\"", key, defaultValue ?? "null");
+            }
+            else {
+                logger.Info("Success! Value is {0}.", val);
+            }
+
+            return val ?? defaultValue;
         }
 
         public static bool GetBool(string key, bool defaultValue = false)
         {
+            logger.Info("Getting config value for \"{0}\"...", key);
             var val = ConfigurationManager.AppSettings[key];
-            return val == null ? defaultValue : bool.Parse(val);
+            if (val == null) {
+                logger.Info("No value found for \"{0}\", defaulting value to {1}.", key, defaultValue);
+            }
+            else {
+                logger.Info("Success! Value is {0}.", val);
+            }
+
+            return val != null ? bool.Parse(val) : defaultValue;
         }
 
         public static void Set(string key, string value)
