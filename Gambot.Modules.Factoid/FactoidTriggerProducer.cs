ec75520ef22466dc9f911cb083c00854e7c7233c
Mohammed Hossain <mohammedh123@gmail.com>
2014-11-19 14:36:56 -0500

    Adds support for alias handling in FactoidTriggerProducer.

--- Gambot.Modules.Factoid/FactoidTriggerProducer.cs
+++ Gambot.Modules.Factoid/FactoidTriggerProducer.cs
@@ -26,31 +26,34 @@ namespace Gambot.Modules.Factoid
                   int.Parse(Config.Get("FactoidTriggerLength"))))
                 return null;
 
-            var randomReply = dataStore.GetRandomValue(message.Text);
-            if (randomReply == null)
-                return null;
+            return ProcessFactoid(message.Text, message);
+        }
 
-            var factoid =
-                FactoidUtilities.GetVerbAndResponseFromPartialFactoid(
-                    randomReply);
-            factoid.Trigger = message.Text;
-
-            var factoidResponse = variableHandler.Substitute(factoid.Response,
-                                                             message);
-
-            switch (factoid.Verb)
+        private ProducerResponse ProcessFactoid(string messageText, IMessage message)
+        {
+            while (true)
             {
-                case "reply":
-                    return new ProducerResponse(factoidResponse, false);
-                case "action":
-                    return new ProducerResponse(factoidResponse, true);
-                default:
-                    return
-                        new ProducerResponse(String.Format("{0} {1} {2}",
-                                                           message.Text,
-                                                           factoid.Verb,
-                                                           factoid.Response),
-                                             false);
+                var randomReply = dataStore.GetRandomValue(messageText);
+                if (randomReply == null)
+                    return null;
+
+                var factoid = FactoidUtilities.GetVerbAndResponseFromPartialFactoid(randomReply);
+                factoid.Trigger = message.Text;
+
+                var factoidResponse = variableHandler.Substitute(factoid.Response, message);
+
+                switch (factoid.Verb)
+                {
+                    case "reply":
+                        return new ProducerResponse(factoidResponse, false);
+                    case "action":
+                        return new ProducerResponse(factoidResponse, true);
+                    case "alias":
+                        messageText = factoid.Response;
+                        continue;
+                    default:
+                        return new ProducerResponse(String.Format("{0} {1} {2}", message.Text, factoid.Verb, factoid.Response), false);
+                }
             }
         }
     }
