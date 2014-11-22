7cb2bf02900ec94203ebe945519c32b972d50fdf
Mohammed Hossain <mohammedh123@gmail.com>
2014-11-21 21:06:25 -0500

    Adds the IgnoreModule class to the ignore module.

---
+++ Gambot.Modules.Ignore/IgnoreModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gambot.Core;
+
+namespace Gambot.Modules.Ignore
+{
+    public class IgnoreModule : AbstractModule
+    {
+        public IgnoreModule()
+        {
+        }
+    }
+}
