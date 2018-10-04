bfe2839cfc02d95763c9f6213bca55d3676f3912
Wilbert Chen <wc655@nyu.edu>
2018-10-04 03:01:52 -0400

    Converted Simon's code for use

---
+++ Gambot.Modules.Roll/DiceEvaluator.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gambot.Modules.Roll
+{
+    public class DiceToken
+    {
+        public bool IsNumber { get; set; }
+        public string StringValue { get; set; }
+        public int IntValue { get; set; }
+    }
+
+    public class DiceExpressionResults
+    {
+        public bool IsValid { get; set; } = false;
+        public float Result { get; set; } = 0;
+        public List<string> RollDescriptionList { get; set; } = new List<string>();
+        public string IndividualRollResultString { get; set; } = null;
+    }
+
+    /*
+     * Credits to Simon Gellis for writing the typescript version
+     * https://gist.github.com/SupernaviX/2a4ee385026ccc6566203db6c7464f01
+     */
+    public class DiceEvaluator
+    {
+        private static Random _RNGesus = new Random();
+
+        private DiceExpressionResults _results;
+
+        public DiceEvaluator()
+        {
+            _results = new DiceExpressionResults();
+        }
+
+        private int RollDie(int numberOfSides)
+        {
+            return _RNGesus.Next(numberOfSides) + 1;
+        }
+
+        private IEnumerable<int> RollDice(int numberOfDice, int numberOfSides)
+        {
+            List<int> rolls = new List<int>();
+            for(var i = 0; i < numberOfDice; ++i)
+                rolls.Add(RollDie(numberOfSides));
+
+            var rollsCSV = String.Join(", ", rolls.Select(k => Convert.ToInt32(k)));
+            _results.RollDescriptionList.Add($" (Rolled: {rollsCSV})");
+
+            return rolls;
+        }
+
+        private IEnumerable<int> KeepHighest(int numberOfDice, int numberOfSides, int? numberToKeep = null)
+        {
+            IEnumerable<int> rolls = RollDice(numberOfDice, numberOfSides);
+            return rolls.OrderByDescending(r => r).Take((numberToKeep == null) ? numberOfDice : numberToKeep.Value);
+        }
+
+        private IEnumerable<int> KeepLowest(int numberOfDice, int numberOfSides, int numberToKeep)
+        {
+            IEnumerable<int> rolls = RollDice(numberOfDice, numberOfSides);
+            return rolls.OrderBy(r => r).Take(numberToKeep);
+        }
+
+        private string _Symbols = "khld*/+-()";
+        private IEnumerable<DiceToken> Tokenize(string expression)
+        {
+            List<DiceToken> tokens = new List<DiceToken>();
+            var expr = expression.ToLower();
+
+            var i = 0;
+            while (i < expr.Length)
+            {
+                if(_Symbols.Contains(expr[i]))
+                {
+                    tokens.Add(new DiceToken() { IsNumber = false, StringValue = expr[i].ToString() });
+                }
+                else if (Char.IsDigit(expr[i]))
+                {
+                    var start = i;
+                    do { ++i; } while (i < expr.Length && Char.IsDigit(expr[i]));
+                    tokens.Add(new DiceToken() { IsNumber = true, IntValue = Convert.ToInt32(expr.Substring(start, i - start)) });
+                    continue;
+                }
+                ++i;
+            }
+
+            return tokens;
+        }
+
+        private bool IsHigherPrecedence(string first, string second)
+        {
+            var precString =_Symbols.Substring(0, _Symbols.Length - 2);
+            var firstPrec = precString.IndexOf(first);
+            var secondPrec = precString.IndexOf(second);
+            return firstPrec <= secondPrec;
+        }
+
+        private int NumberOfArgumentsForSymbol(string symbol)
+        {
+            return (symbol == "k" || symbol == "h" || symbol == "l") ? 3 : 2;
+        }
+
+        private IEnumerable<DiceToken> ConvertToReversePolishNotation(string expression)
+        {
+            var stack = new Stack<DiceToken>();
+            var rpn = new Queue<DiceToken>();
+
+            var tokens = Tokenize(expression);
+            foreach (var token in tokens)
+            {
+                if (token.IsNumber)
+                {
+                    rpn.Enqueue(token);
+                }
+                else if (token.StringValue == "(")
+                {
+                    stack.Push(token);
+                }
+                else if (token.StringValue == ")")
+                {
+                    while (stack.ElementAt(stack.Count - 1).StringValue != "(")
+                    {
+                        rpn.Enqueue(stack.Pop());
+                    }
+                    stack.Pop();
+                }
+                else
+                {
+                    while (
+                        stack.Count > 0
+                        && IsHigherPrecedence(stack.ElementAt(stack.Count - 1).StringValue, token.StringValue)
+                        && stack.ElementAt(stack.Count - 1).StringValue != "("
+                    )
+                    {
+                        rpn.Enqueue(stack.Pop());
+                    }
+                    if (NumberOfArgumentsForSymbol(token.StringValue) == 3)
+                    {
+                        // throw out extra token for ternary operators
+                        stack.Pop();
+                    }
+                    stack.Push(token);
+                }
+            }
+
+            while (stack.Count > 0)
+            {
+                rpn.Enqueue(stack.Pop());
+            }
+
+            return rpn;
+        }
+
+        public DiceExpressionResults Evaluate(string expression)
+        {
+            _results = new DiceExpressionResults();
+            try
+            {
+                var numStack = new Stack<float>();
+                foreach (var token in ConvertToReversePolishNotation(expression))
+                {
+                    if (token.IsNumber)
+                    {
+                        numStack.Push(token.IntValue);
+                        continue;
+                    }
+                    else
+                    {
+                        var arguments = new Stack<float>();
+                        for (var i = 0; i < NumberOfArgumentsForSymbol(token.StringValue); ++i)
+                        {
+                            arguments.Push(numStack.Pop());
+                        }
+
+                        float calc = 0;
+                        switch (token.StringValue)
+                        {
+                            case "h":
+                            case "k":
+                            case "l":
+                                var keep = (token.StringValue != "l") ? 
+                                    KeepHighest((int)arguments.Pop(), (int)arguments.Pop(), (int)arguments.Pop())
+                                    : KeepLowest((int)arguments.Pop(), (int)arguments.Pop(), (int)arguments.Pop());
+                                var keepCSV = String.Join(", ", keep.Select(k => Convert.ToInt32(k)));
+                                _results.RollDescriptionList.Add($"(Kept: {keepCSV})");
+                                calc = keep.Sum();
+                                break;
+                            case "d":
+                                var rolls = RollDice((int)arguments.Pop(), (int)arguments.Pop());
+                                calc = rolls.Sum();
+                                break;
+                            case "*":
+                                calc = arguments.Pop() * arguments.Pop();
+                                break;
+                            case "/":
+                                calc = arguments.Pop() / arguments.Pop();
+                                break;
+                            case "+":
+                                calc = arguments.Pop() + arguments.Pop();
+                                break;
+                            case "-":
+                                calc = arguments.Pop() - arguments.Pop();
+                                break;
+                            default: break;
+                        }
+
+                        numStack.Push(calc);
+                    }
+                }
+
+                _results.IsValid = true;
+                _results.Result = numStack.ElementAt(0);
+                _results.IndividualRollResultString = String.Join("", _results.RollDescriptionList).Trim();
+
+                return _results;
+            }
+            catch (Exception e)
+            {
+                _results.IsValid = false;
+                return _results;
+            }
+        }
+    }
+}
--- Gambot.Modules.Roll/RollResponseProducer.cs
+++ Gambot.Modules.Roll/RollResponseProducer.cs
@@ -13,7 +13,7 @@ namespace Gambot.Modules.Roll
         private readonly IVariableHandler variableHandler;
 
         private const string DefaultRollSuccessReply =
-            "<reply> $who rolled a $diceRoll.";
+            "<reply> $who rolled a $diceRoll. $diceCast";
         private const string DefaultRollFailedReply =
             "<reply> That's a leaner.";
 
@@ -35,48 +35,30 @@ namespace Gambot.Modules.Roll
                 var match = Regex.Match(trimmedMessage, "roll (.+)");
                 if (match.Success)
                 {
-                    var diceSyntaxRegex = @"^(\d+)[dD](\d+)$|^(\d+)$";
-                    var rolls = match.Groups[1].Value.Split('+').Select(roll => roll.Trim());
-                    bool validExpressions = rolls.All(
-                        roll => 
-                        {
-                            var diceSyntax = Regex.Match(roll, diceSyntaxRegex);
-                            return diceSyntax.Success;
-                        }
-                    );
-                    if (!validExpressions)
+                    var originalQuery = match.Groups[1].Value.Trim();
+
+                    DiceEvaluator d = new DiceEvaluator();
+                    var results = d.Evaluate(originalQuery);
+
+                    var sumOfRolls = results.Result;
+                    var validExpression = results.IsValid;
+                    var rollsDescription = results.IndividualRollResultString;
+
+                    if (!validExpression)
                     {
                         var rollFailedFactoidStr = factoidDataStore.GetRandomValue("dice roll failed reply")?.Value ?? DefaultRollFailedReply;
                         var rollFailedFactoid = FactoidUtilities.GetVerbAndResponseFromPartialFactoid(rollFailedFactoidStr);
                         return new ProducerResponse(variableHandler.Substitute(rollFailedFactoid.Response, message), false);
                     }
 
-                    Random rngesus = new Random();
-                    int sumOfRolls = rolls.Sum(
-                        roll =>
-                        {
-                            var diceSyntax = Regex.Match(roll, diceSyntaxRegex);
-
-                            var numDice = diceSyntax.Groups[1];
-                            var valueRange = diceSyntax.Groups[2];
-                            var constValue = diceSyntax.Groups[3];
-
-                            var total = (constValue.Success) ? Convert.ToInt32(constValue.Value) : 0;
-                            if (!constValue.Success)
-                            {
-                                for (int i = 0; i < Convert.ToInt32(numDice.Value); ++i)
-                                {
-                                    total += rngesus.Next(Convert.ToInt32(valueRange.Value) + 1);
-                                }
-                            }
-                            return total;
-                        }
-                    );
-
                     var rollSuccessFactoidStr = factoidDataStore.GetRandomValue("dice roll success reply")?.Value ?? DefaultRollSuccessReply;
                     var rollSuccessFactoid = FactoidUtilities.GetVerbAndResponseFromPartialFactoid(rollSuccessFactoidStr);
-                    var coercedResponse = Regex.Replace(rollSuccessFactoid.Response, @"\$(?:diceRoll)", sumOfRolls.ToString(), RegexOptions.IgnoreCase);
-                    return new ProducerResponse(variableHandler.Substitute(coercedResponse, message), false);
+
+                    var replaceForOriginal = Regex.Replace(rollSuccessFactoid.Response, @"\$(?:diceQuery)", originalQuery, RegexOptions.IgnoreCase);
+                    var replaceForIndividuals = Regex.Replace(replaceForOriginal, @"\$(?:diceCast)", rollsDescription, RegexOptions.IgnoreCase);
+                    var replaceForTotal = Regex.Replace(replaceForIndividuals, @"\$(?:diceRoll)", sumOfRolls.ToString(), RegexOptions.IgnoreCase);
+
+                    return new ProducerResponse(variableHandler.Substitute(replaceForTotal, message), false);
                 }
             }
             return null;
